db1d0b0293a6343d15cbdd3f3184d33d01605bc1
Balint831 <47824923+Balint831@users.noreply.github.com>
2019-03-05 15:37:34 +0100

    3.2

--- 3_gyak_hf/newton_negyzet+template/main.cpp
+++ 3_gyak_hf/newton_negyzet+template/main.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 
-template<typename F, typename G, typename T>
-T newton(F f, G g, T x0){
+template<typename F, typename G, typename T, typename H>
+T newton(F f, G g, T x0, H limit){
     double a;
     for (;;) {
         a = x0 - f(x0)/g(x0);
-        if (fabs(x0-a)<1E-12){
+        if (fabs(x0-a)<limit){
             return a;
         }
         x0 = a;
@@ -18,6 +18,8 @@ T newton(F f, G g, T x0){
 int main()
 {
     double a;
-    a = newton([](double x){return x * x - 612.0; }, [](double x){return 2.0 * x; }, 10.0);
+    a = newton([](double x){return x * x - 612.0; },
+               [](double x){return 2.0 * x; }, 10.0,1E-12);
+    std::cout.precision(16);
     std::cout << a << endl;
 }
