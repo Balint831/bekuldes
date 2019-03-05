4a01683c3d373a1c59524b5e7aee9f99c62ab4ed
Balint831 <47824923+Balint831@users.noreply.github.com>
2019-03-05 19:42:44 +0100

    Update main.cpp

--- 2_gyak_hf/newton_cotes/main.cpp
+++ 2_gyak_hf/newton_cotes/main.cpp
@@ -3,13 +3,14 @@
 
 using namespace std;
 
+template<typename T>
+T sq(T x){return x*x;}
 
+double f(double x){return cos(x)*std::exp(-1*sq(x));}
 
-double f(double x){
-    return cos(x)*std::exp(-1*std::pow(x,2));
-}
 //n>0, a>b
 double integrate(int n, double x0, double x1){
+    if (n <=0){ cout<<"Hiba: n<=0\n"; return 1;}
     if (x0>x1){std::swap(x0,x1);
         std::cout << "\nA hatarok rosszul lettek megadva: x1<x0. Megcsereltem oket!\n" <<endl;}//megcserélés
     double h = (x1-x0)/(n+2);
@@ -20,20 +21,10 @@ double integrate(int n, double x0, double x1){
     return 2*h*q;
 }
 
-/*double integrate(int n, double x0, double x1){
-    double h = (x1-x0)/n;
-    double q;
-    double xj;
-    for(int j = 1; j<=n-1; j++){
-        q += f(x0 + j*h);
-    }
-    return h/2 * (f(x0) + 2*q + f(x1));
-}*/
-
 int main()
 {
     std::cout.precision(16);
-    double z = integrate(25000000,3,-1);
+    double z = integrate(250000,-1,3);
 
     std::cout <<"Eredmenye: " << z << "\n"
         << "integrate(cos(x)*e^(-x^2), -1,3) a WolframAlpha szerint: 1.34638795680345037669816 \nA ketto kulonbsege: "
