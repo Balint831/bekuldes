1978ba81fb4bc798ff261e226c8477c587fdc0b1
Balint831 <47824923+Balint831@users.noreply.github.com>
2019-03-19 20:22:32 +0100

    :100: :100: :fire: :fire:

--- 4_gyak_hf/egyenes.cpp
+++ 4_gyak_hf/egyenes.cpp
@@ -4,16 +4,19 @@
 #include <array>
 #include <cmath>
 
-template<typename T>
-T sq(T x){ return x*x; }
 
 std::array<double, 2> parameters(std::vector<double> const& x, std::vector<double> const& y){
     if ( x.size() != y.size() ){
         std::cout << "Error: size of X and Y don't match. \n";
         return {NAN,NAN};
     }
-    double sum_x = std::accumulate(x.begin(), x.end(), 0);
-    double sum_y = std::accumulate(y.begin(), y.end(), 0);
+    if ( x.size() == 0)
+    {
+        return {NAN,NAN};
+    }
+
+    double sum_x = std::accumulate(x.begin(), x.end(), 0.0);
+    double sum_y = std::accumulate(y.begin(), y.end(), 0.0);
     int i = static_cast<int>(x.size());
     
     double mean_x = sum_x/i;
@@ -22,42 +25,44 @@ std::array<double, 2> parameters(std::vector<double> const& x, std::vector<doubl
     double b_numer = 0;
     double b_denom = 0;
     
-    for(int j = 0; j < i; j++){
-        b_numer += (x[j] - mean_x) * (y[j] - mean_y);
-        b_denom += sq(x[j] - mean_x);
-    }
+    auto f = [mean_x,mean_y](double a, double b){ return (a - mean_x) * (b - mean_y);};
+    auto g = [](double a, double b){ return a + b*b;};
+    b_numer = std::inner_product(x.begin(), x.end(), y.begin(), 0.0, std::plus<double>(), f);
+    b_denom = std::accumulate(x.begin(), x.end(), 0.0, g);
+    //for(int j = 0; j < i; j++){
+        //b_numer += (x[j] - mean_x) * (y[j] - mean_y);
+    //}
+    
     double b = b_numer/b_denom;
     double m = mean_y - (b * mean_x);
     return {b,m};
 }
 
 int main(){
+    //Test case: straight line connecting 2 points
+    std::vector<double> u = {0, 1};
+    std::vector<double> v = {0, 2};
+    std::array<double, 2> param = parameters(u,v);
+    std::cout <<"y = " << param[0] << " * x + " << param[1] <<std::endl;
 
+    //Test case: non-matching vector size
+    u = {0, 1};
+    v = {0};
+    param = parameters(u,v);
+    std::cout <<"y = " << param[0] << " * x + " << param[1] <<std::endl;
+
+    //Numbers scattered around a straight line: y = 3.9*x-4.777+rand
     std::vector<double> X = {-10.,  -9.,  -8.,  -7.,  -6.,  -5.,  -4.,  -3.,  -2.,  -1.,   0.,
          1.,   2.,   3.,   4.,   5.,   6.,   7.,   8.,   9.,  10.};
-    std::vector<double> Y = {-40.84402621, -34.44268102, -30.18946895, -30.32277281,
-       -23.46251648, -20.6829044 , -15.90160359, -13.76817768,
-        -7.82667235,  -1.10506376,  -3.48847497,  -0.56390248,
-         5.43916348,  11.99643555,  17.34254311,  21.2161478 ,
-        21.49048847,  24.65790221,  30.20248365,  33.02920248,
-        40.02040066};
+    std::vector<double> Y = {-38.66244597, -28.95488426, -29.7711778 , -20.86226482,
+       -36.17297738, -22.68780779, -17.47410452, -15.82897239,
+       -12.64531047,   8.4791807 ,  -3.54591447,   4.81745921,
+         6.07552966,   3.97511625,   9.09218524,  16.39128824,
+        18.57345306,  19.07712847,  31.87348948,  33.53273962,
+        40.31253216};
     
-    //Numbers scattered around a straight line: y = 3.9*x-4.777+rand
-    std::array<double, 2> param = parameters(X,Y);
-    std::cout <<"y = " << param[0] << " * x + " << param[1] <<std::endl;
     
-    //Test case: non-matching vector size
-    std::vector<double> u = {0, 1};
-    std::vector<double> v = {0};
-    param = parameters(u,v);
+    param = parameters(X,Y);
     std::cout <<"y = " << param[0] << " * x + " << param[1] <<std::endl;
-    
-    //Test case: straight line connecting 2 points
-    u = {0, 1};
-    v = {0, 2};
-    param = parameters(u,v);
-    std::cout <<"y = " << param[0] << " * x + " << param[1] <<std::endl;
-
-
     return 0;
 }
